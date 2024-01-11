fbfffaeb539adf4390677b4e78c2c99e933f39fa
kornbinson <kornbinson@gmail.com>
2024-01-11 10:47:34 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeployWebAppEc2ChatRoom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
